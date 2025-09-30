3ea2ffe995fb759471fa4a443e9caeb67b93ddf9
Najwanlaskar <Najwanlaskar12@example.com>
2025-09-30 19:32:32 +0100

    modified index.html and script.js
    connect buttons to const in js

---
+++ script.js
@@ -0,0 +1,23 @@
+//section1
+const Clearbtn=document.querySelector(".clear");
+const MultiplyBtn =document.querySelector(".symbolX");
+const DivisionBtn =document.querySelector(".symbol÷");
+const BackspaceBtn =document.querySelector(".backspace");
+//section2
+const Btn1=document.querySelector(".btn1");
+const Btn2=document.querySelector(".btn2");
+const Btn3=document.querySelector(".btn3");
+const AdditionBtn=document.querySelector(".symbol+");
+//section3
+const Btn4=document.querySelector(".btn4");
+const Btn5=document.querySelector(".btn5");
+const Btn6=document.querySelector(".btn6");
+const SubstractBtn=document.querySelector(".symbol-");
+//section4
+const Btn7=document.querySelector(".btn7");
+const Btn8=document.querySelector(".btn8");
+const Btn9=document.querySelector(".btn9");
+const EqualBtn=document.querySelector(".equal");
+//section5
+const Btn0=document.querySelector(".btn0");
+const DecimalBtn=document.querySelector(".decimal");
